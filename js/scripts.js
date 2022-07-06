ba4ad5bb0eee444e4966ecc1d7975777898d9a14
LAPTOP-EPK5CUF8\Silvio <sgpalacios@institutosanmartin.edu.ar>
2022-07-06 16:11:44 -0300

    Cambio: 5

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+/* const div = document.getElementById("navbarCollapse")
+
+const boton = document.getElementById("boton");
+
+var cont = 0;
+
+function fucionAgrandar(cont){
+    if (cont == 0) 
+    {
+        div.style.height = "220px";
+        cont = 1;
+    }
+    else{
+        div.style.height = "70px";
+        cont = 0;
+    }
+}
+
+boton.addEventListener("click",function(){
+    fucionAgrandar();
+}) */
+
+
+
+
